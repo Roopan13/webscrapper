56dd1e86e62f4c54b05755801428007f166ead63
Roopan Sharma <sharmaroopan7@gmail.com>
2022-03-26 12:54:16 -0400

    Second commit

--- webscrapper/settings.py
+++ webscrapper/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from ctypes import cast
+from email.policy import default
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&%6w2!8!q)l55-3^@$#@obc31(epdls+9u*hsy+byqfmkcw3=z'
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool,default = True)
 
 ALLOWED_HOSTS = []
 
